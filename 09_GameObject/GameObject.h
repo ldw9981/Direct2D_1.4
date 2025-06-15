e4eefe2581f72fa63a0fa1942192c87ab2a521ef
Lee DongWon <ldw9981@gmail.com>
2025-06-15 23:39:04 +0900

    설명추가

--- 09_GameObject/GameObject.h
+++ 09_GameObject/GameObject.h
@@ -15,10 +15,14 @@ public:
         components.clear();
     }
 
+    // Class T를 생성하는 함수 , 인자까지 전달한다.
     template<typename T, typename... Args>
-    T* AddComponent(Args&&... args) {
+    T* AddComponent(Args&&... args) 
+    {
+        // 컴파일 시점에 T가 Component를 상속받은 클래스 인지 확인
         static_assert(std::is_base_of<Component, T>::value, "T must derive from Component");
 
+        // 인자까지 전달하면서 생성
         T* comp = new T(std::forward<Args>(args)...);
         comp->owner = this;
         components.push_back(comp);
@@ -29,6 +33,8 @@ public:
     std::vector<T*> GetComponents() {
         std::vector<T*> result;
         for (Component* comp : components) {
+            // 실행도중(Runtime)에 comp가 가르키는 인스턴스가 RTTI정보를 이용하여 
+            // T이거나 T의 자식 클래스 이면 주소를 리턴한다.
             if (auto casted = dynamic_cast<T*>(comp))
                 result.push_back(casted);
         }
